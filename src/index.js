b061fbbea7dfb441dbf7b738f22cdd2b01f5b578
lwjj <848565635@qq.com>
2019-09-19 20:49:16 +0800

    todolist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './App'
+import TodoList from './TodoList'
     ReactDom.render(
-        <App/>,
+        <TodoList/>,
         document.getElementById('root')
     )
