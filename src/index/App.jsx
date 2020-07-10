b93eb9f5a2e7fc3484b5f5be3b2526b67066608e
lwjj <848565635@qq.com>
2020-07-10 23:24:06 +0800

    开发城市选择浮层

---
+++ src/index/App.jsx
@@ -0,0 +1,81 @@
+import React, { useCallback, useMemo } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import './App.css'
+
+import Header from '../common/Header.jsx'
+import DepartDate from './DepartDate.jsx'
+import HighSpeed from './HighSpeed.jsx'
+import Journey from './Journey.jsx'
+import Submit from './Submit.jsx'
+import CitySelector from '../common/CitySelector.jsx'
+
+import {
+    exchangeFromTo,
+    showCitySelector,
+    hideCitySelector
+} from './actions'
+
+function App(props) {
+    const {
+        from,
+        to,
+        isCitySelectorVisible,
+        cityData,
+        isLoadingCityData,
+        dispatch 
+    } = props
+    const onBack = useCallback(() => {
+        window.history.back()
+    },[])
+
+    const cbs = useMemo(() => {
+        return bindActionCreators({
+            exchangeFromTo,
+            showCitySelector
+        }, dispatch)
+    }, [])
+
+    const citySelectorCbs = useMemo(() => {
+        return bindActionCreators({
+            onBack: hideCitySelector
+        }, dispatch)
+    }, [])
+
+
+    return (
+        <div>
+            <div className='header-wrapper'>
+                <Header title='火车票' onBack={ onBack }/>
+            </div>  
+            <form className='form'>
+                <Journey 
+                    from={from} 
+                    to={to}
+                    {...cbs}
+                />
+                <DepartDate/>
+                <HighSpeed/>
+                <Submit/>
+
+                <CitySelector
+                    show={isCitySelectorVisible}
+                    cityData={cityData}
+                    isLoading={isLoadingCityData}
+                    {...citySelectorCbs}
+                />
+            </form>
+        </div>
+    )
+}
+
+export default connect(
+    function mapStateToProps(state) {
+        return state
+    },
+    function mapDispatchToProps(dispatch) {
+        return {
+            dispatch
+        }
+    }
+)(App)
---
+++ src/index/store.js
@@ -0,0 +1,23 @@
+import {
+    createStore,
+    combineReducers,
+    applyMiddleware
+} from 'redux'
+
+import reducers from './reducers'
+import thunk from 'redux-thunk'
+
+export default createStore(
+    combineReducers(reducers),
+    {
+        form: '北京',
+        to: '上海',
+        isCitySelectorVisible: false,
+        currentSelectingLeftCity: false,
+        cityData: null,
+        isLoadingCityData: false,
+        isDateSelectorVisible: false,
+        highSpeed: false,
+    },
+    applyMiddleware(thunk)
+)
